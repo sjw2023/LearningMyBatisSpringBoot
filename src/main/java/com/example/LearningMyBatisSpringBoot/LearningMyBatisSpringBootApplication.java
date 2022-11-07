9fc43d63c481bbaa749e9b448553f87c3d819774
JoowonSuh <sjw2023@gmail.com>
2022-11-08 00:37:55 +0900

    initial commit

---
+++ src/main/java/com/example/LearningMyBatisSpringBoot/LearningMyBatisSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningMyBatisSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMyBatisSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningMyBatisSpringBootApplication.class, args);
+	}
+
+}
